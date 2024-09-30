38220a1a3e93ee733b2ce58a7750437a723dbfde
PETRUSJEFFREY <142968661+PETRUSJEFFREY@users.noreply.github.com>
2024-09-30 09:49:34 +0530

    Update Recursion Basics.java

--- Recursion Basics.java
+++ Recursion Basics.java
@@ -63,3 +63,20 @@ public class Main {
     }
     
 }
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      toh(3,'A','B','C');
+  }
+  public static void toh(int n,char sr,char aux,char des){
+    if(n==1) System.out.println("Move disk "+ sr+" to "+des);
+    else{
+      toh(n-1,sr,des,aux);
+      System.out.println("Move disk "+ sr+" to "+des);
+      toh(n-1,aux,sr,des);
+
+      
+    }
+  }
+}
